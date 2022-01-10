861dd45568ba376f33ab78644b80f36e8df92cb6
husenindia <husentelwala@gmail.com>
2022-01-10 17:25:38 +0530

    Home page design and linking the social connectivity

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faStackOverflow,faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public name;
+  name;
+  imagePath;
+  faStackOverflow;
+  faGithub;
+  faGlobe;
   constructor() {
     this.name="Husen Telwala";
+    this.imagePath ="../../assets/images/"
+    this.faStackOverflow = faStackOverflow;
+    this.faGithub = faGithub;
+    this.faGlobe = faGlobe;
    }
 
   ngOnInit(): void {
