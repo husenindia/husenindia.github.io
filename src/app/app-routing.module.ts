6877e17062578d4185f11372d2e9518b80e7701d
husenindia <husentelwala@gmail.com>
2022-01-10 19:42:07 +0530

    Routing file changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,8 +9,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
+    component: HomeComponent,
   },
   {
     path: 'home',
